bb5ae34db8c0bc91d5acc856d45d18ba7a68fdf5
yllorca <yllorca@helloworld.cl>
2020-01-22 23:26:53 -0300

    se crear vista detalle servicio

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import HomeView, form_contacto_ajax
+from .views import HomeView, form_contacto_ajax, ServicioDetalleView
 
 urlpatterns = [
     url(r'^$', HomeView, name='home'),
+    url(r'^servicio/(?P<slug>[-\w]+)/$', ServicioDetalleView, name='detalle-servicio'),
     url(r'^form/form_ajax$', form_contacto_ajax, name='form-contacto-ajax'),
 ]
